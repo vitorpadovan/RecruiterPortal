ce5b2dfe1f86ccd11c3eecd5f1fcf08dc8151b09
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2025-01-27 08:45:33 -0300

    Corrigindo nome de classes.

---
+++ RecruiterPortal/Controllers/ApplicationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using RecruiterPortal.Business.Interfaces;
+using RecruiterPortal.Controllers.Request.Application;
+using RecruiterPortal.Controllers.Response.Application;
+
+namespace RecruiterPortal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly IApplicationBusiness _applicationBusiness;
+
+        public ApplicationController(IApplicationBusiness applicationBusiness)
+        {
+            _applicationBusiness = applicationBusiness;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([FromBody] NewApplicationRequest newApplicationRequest)
+        {
+            NewApplicationResponse r = await _applicationBusiness.SaveApplication(newApplicationRequest);
+            return Ok(r);
+        }
+    }
+}
---
+++ RecruiterPortal/Model/JobApplicationModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using RecruiterPortal.Controllers.Request.Application;
+using RecruiterPortal.Controllers.Response.Application;
+
+namespace RecruiterPortal.Model
+{
+    public class JobApplicationModel
+    {
+        public Guid Id { get; set; }
+        public required string CompanyName { get; set; }
+        public Uri? ContactUrl { get; set; }
+        public required DateTime ApplicationData { get; set; }
+        public required Uri JobDescriptionUrl { get; set; }
+        public ApplicationTypeEnum ApplicationType { get; set; }
+        public Uri? ApplicationUrl { get; set; }
+
+
+        public static implicit operator JobApplicationModel(NewApplicationRequest v)
+        {
+            return new() { 
+                ApplicationData = DateTime.Now.ToUniversalTime(),
+                CompanyName = v.CompanyName,
+                JobDescriptionUrl = v.JobDescriptionUrl,
+                ContactUrl = v.ContactUrl,
+                ApplicationType = v.ApplicationType,
+                ApplicationUrl = v.ApplicationUrl
+            };
+        }
+    }
+}
