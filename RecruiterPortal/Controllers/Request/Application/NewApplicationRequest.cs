d5bafec2d07bc56c3b1c0460f59ed6cba4c51f60
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2025-01-27 08:47:41 -0300

    Adicionando request e response.

---
+++ RecruiterPortal/Controllers/Request/Application/NewApplicationRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using RecruiterPortal.Model;
+
+namespace RecruiterPortal.Controllers.Request.Application
+{
+    public class NewApplicationRequest
+    {
+        public required string CompanyName { get; set; }
+        public Uri? ContactUrl { get; set; }
+        public required Uri JobDescriptionUrl { get; set; }
+        public ApplicationTypeEnum ApplicationType { get; set; }
+        public Uri? ApplicationUrl { get; set; }
+    }
+}
---
+++ RecruiterPortal/Controllers/Response/Application/NewApplicationResponse.cs
@@ -0,0 +1,30 @@
+﻿using RecruiterPortal.Controllers.Request.Application;
+using RecruiterPortal.Model;
+
+namespace RecruiterPortal.Controllers.Response.Application
+{
+    public class NewApplicationResponse
+    {
+        public Guid Id { get; set; }
+        public required string CompanyName { get; set; }
+        public Uri? ContactUrl { get; set; }
+        public required DateTime ApplicationData { get; set; }
+        public required Uri JobDescriptionUrl { get; set; }
+        public ApplicationTypeEnum ApplicationType { get; set; }
+        public Uri? ApplicationUrl { get; set; }
+
+        public static implicit operator NewApplicationResponse(JobApplicationModel v)
+        {
+            return new()
+            {
+                Id = v.Id,
+                CompanyName = v.CompanyName,
+                ContactUrl = v.ContactUrl,
+                ApplicationData = v.ApplicationData,
+                JobDescriptionUrl = v.JobDescriptionUrl,
+                ApplicationType = v.ApplicationType,
+                ApplicationUrl = v.ApplicationUrl
+            };
+        }
+    }
+}
