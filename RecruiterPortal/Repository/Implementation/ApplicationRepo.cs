a90a96695f1ca9786249dcd8a09451ea0f69906e
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2025-01-30 21:45:24 -0300

    Arrumando alguns bugs do sonar.

--- RecruiterPortal/Repository/Implementation/ApplicationRepo.cs
+++ RecruiterPortal/Repository/Implementation/ApplicationRepo.cs
@@ -14,17 +14,18 @@ namespace RecruiterPortal.Repository.Implementation
 
         public async Task<JobApplicationModel> SaveApplication(JobApplicationModel newApplicationRequest)
         {
+            _logger.LogInformation("Salvando a aplicação á uma vaga");
             try
             {
                 var result = await _dbSet.AddAsync(newApplicationRequest);
                 await _context.SaveChangesAsync();
+                _logger.LogInformation("Aplicação salva");
                 return result.Entity;
             }
             catch (Exception ex)
             {
-                throw new Exception($"Erro ao tentar salvar o job {newApplicationRequest.CompanyName} com a URL {newApplicationRequest.JobDescriptionUrl}", ex);
+                throw new ApplicationException($"Erro ao tentar salvar o job {newApplicationRequest.CompanyName} com a URL {newApplicationRequest.JobDescriptionUrl}", ex);
             }
-
         }
     }
 }
